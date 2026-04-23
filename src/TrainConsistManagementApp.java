69d8610411acdfe50fc167283f9c8bb15dc36094
rohit-6833 <ra6833@srmist.edu.in>
2026-04-23 23:30:06 +0530

    UC10

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,10 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-public class TrainConsistManagementApp
-{
+public class TrainConsistManagementApp{
 
     static class Bogie {
         String name;
@@ -23,7 +20,7 @@ public class TrainConsistManagementApp
 
     public static void main(String[] args) {
         System.out.println("=========================================================");
-        System.out.println(" UC9 - Group Bogies by Type ");
+        System.out.println(" UC10 - Count Total Seats in Train ");
         System.out.println("=========================================================\n");
 
         List<Bogie> bogies = new ArrayList<>();
@@ -31,25 +28,17 @@ public class TrainConsistManagementApp
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 24));
         bogies.add(new Bogie("Sleeper", 70));
-        bogies.add(new Bogie("AC Chair", 60));
 
-        System.out.println("All Bogies:");
+        System.out.println("Bogies in Train:");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
 
-        Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.name));
+        int totalCapacity = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        System.out.println("\nGrouped Bogies:\n");
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println("Bogie Type: " + entry.getKey());
-            for (Bogie b : entry.getValue()) {
-                System.out.println("  Capacity -> " + b.capacity);
-            }
-            System.out.println();
-        }
-
-        System.out.println("UC9 grouping completed...");
+        System.out.println("\nTotal Seating Capacity of Train: " + totalCapacity);
+        System.out.println("\nUC10 aggregation completed...");
     }
 }
